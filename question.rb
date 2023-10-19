a77e52710a50872268dc933b1aa6e73a32d755b9
carolineolagunju <olamidejr21@gmail.com>
2023-10-18 23:12:29 -0600

    implement class question, method to_s and correct?

---
+++ question.rb
@@ -0,0 +1,17 @@
+class Question
+  attr_reader :num1, :num2, :answer
+
+  def initialize
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @answer = num1 + num2
+  end
+  
+  def to_s
+    "What does #{num1} plus #{num2} equal?"
+  end
+  
+  def correct?(player_answer)
+    player_answer == answer
+  end
+end
